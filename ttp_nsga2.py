7a26ea446055deef16439080c98075ea053d2aae
draeson <Draesonfang@gmail.com>
2024-12-28 19:26:05 +0000

    NSGA-II code for TTP: Version 2.0 (NSGA-II algorithm)

--- ttp_nsga2.py
+++ ttp_nsga2.py
@@ -1,17 +1,8 @@
-"""
-This is a code using NSGA-II to solve this problem.
-
-Code by Draeson.
-"""
 import pandas as pd
 import numpy as np
 import random
-import matplotlib.pyplot as plt
-import re
 from typing import List, Tuple, Dict
-import random
 
-from torch.distributed.tensor import empty
 
 
 def load_all_data(file_path):
@@ -65,24 +56,7 @@ def load_all_data(file_path):
 
     return metadata, city_coordinates, items_in_cities
 
-
-# File path
-file_path = "data_resources/test-example-n4.txt"
-
-# Load the data from the file
-metadata, city_coords, city_items = load_all_data(file_path)
-
-# Print results
-print("Metadata:", metadata)
-# print("\nCity Coordinates:", city_coords)
-city_coords = pd.DataFrame.from_dict(city_coords,orient='index',columns=['x','y'])
-print(city_coords)
-# print("\nItems in Cities:", city_items)
-items_info = pd.DataFrame(city_items,columns=['item_id','city_id','value','weight'])
-print(items_info)
-
-
-class NEGA2:
+class NSGA2:
     def __init__(self, metadata: Dict, coordinates: pd.DataFrame, items: pd.DataFrame):
         self.metadata = metadata
         self.coordinates = coordinates
@@ -93,7 +67,7 @@ class NEGA2:
         self.max_speed = metadata['max_speed']
         self.renting_ratio = metadata['renting_ratio']
 
-        # GA parameters
+        # NSGA-II parameters
         self.pop_size = 50
         self.generations = 100
         self.mutation_rate = 0.1
@@ -108,17 +82,15 @@ class NEGA2:
     def calculate_speed(self, current_weight: float) -> float:
         """Calculate current speed based on weight"""
         normalized_weight = current_weight / self.capacity
-        return max(self.min_speed,
-                   self.max_speed - normalized_weight * (self.max_speed - self.min_speed))
+        return max(self.min_speed, self.max_speed - normalized_weight * (self.max_speed - self.min_speed))
 
-    def evaluate_solution(self, tour: List[int], picking_plan: List[bool]) -> float:
-        """Evaluate a complete TTP solution"""
+    def evaluate_solution(self, tour: List[int], picking_plan: List[bool]) -> Tuple[float, float]:
+        """Evaluate a complete TTP solution and return profit and time"""
         total_profit = 0
         current_weight = 0
         total_time = 0
         current_city = tour[0]
 
-        # Create a mapping of cities to items and their picking status
         picked_items = {}
         for i, (_, item) in enumerate(self.items.iterrows()):
             if item['city_id'] not in picked_items:
@@ -126,27 +98,163 @@ class NEGA2:
             picked_items[item['city_id']].append((picking_plan[i], item))
 
         for next_city in tour[1:] + [tour[0]]:  # Return to start
-            # Pick items at current city
             if current_city in picked_items:
                 for pick, item in picked_items[current_city]:
-                    if pick:
+                    if (current_weight + item['weight']) > self.capacity:
+                        picked_items[current_city][0] = (False,) + picked_items[current_city][0][1:]
+                    else:
                         current_weight += item['weight']
                         total_profit += item['value']
 
-                        # Check if weight exceeds capacity
-                        if current_weight > self.capacity:
-                            return float('-inf')  # Invalid solution
-
-            # Travel to next city
             distance = self.calculate_distance(current_city, next_city)
             speed = self.calculate_speed(current_weight)
             total_time += distance / speed
 
             current_city = next_city
 
-        # Calculate objective value
-        objective = total_profit - (self.renting_ratio * total_time)
-        return objective
+        return total_profit, total_time
+    def dominated(self,a1,b1,a2,b2) -> bool:
+        return a1 >= b1 and a2 <= b2 and (a1 > b1 or a2 < b2)
+
+    def fast_nondominated_sort(self, population: List[Tuple[List[int], List[bool]]], fitnesses: List[Tuple[float, float]]):
+        """Perform fast non-dominated sorting and return Pareto fronts"""
+        fronts = [[]]
+        domination_count = [0] * len(population)
+        dominated_solutions = [[] for _ in range(len(population))]
+
+        for p in range(len(population)):
+            for q in range(len(population)):
+                if self.dominated(fitnesses[p][0],fitnesses[q][0],fitnesses[p][1],fitnesses[q][1]):
+                    dominated_solutions[p].append(q)
+                elif self.dominated(fitnesses[q][0],fitnesses[p][0],fitnesses[q][1],fitnesses[p][1]):
+                    domination_count[p] += 1
+
+            if domination_count[p] == 0:
+                fronts[0].append(p)
+
+        while len(fronts[-1]) > 0:
+            next_front = []
+            for p in fronts[-1]:
+                for q in dominated_solutions[p]:
+                    domination_count[q] -= 1
+                    if domination_count[q] == 0:
+                        next_front.append(q)
+            fronts.append(next_front)
+
+        return fronts[:-1]
+
+    def calculate_crowding_distance(self, front: List[int], fitnesses: List[Tuple[float, float]]) -> List[float]:
+        """Calculate crowding distance for a Pareto front"""
+        distance = [[0] * len(front)]*2
+        final_distance = [0] * len(front)
+        num_objectives = len(fitnesses[0])
+
+        for i in range(num_objectives):
+            sorted_front = sorted(range(len(front)), key=lambda x: fitnesses[front[x]][i])
+            distance[i][sorted_front[0]] = float('inf')
+            distance[i][sorted_front[-1]] = float('inf')
+
+            for j in range(1, len(sorted_front) - 1):
+                distance[i][sorted_front[j]] += (fitnesses[front[sorted_front[j + 1]]][i] - fitnesses[front[sorted_front[j - 1]]][i])
+        for i in range(len(final_distance)):
+            final_distance[i] = distance[0][i] + distance[1][i]
+
+        return final_distance
+
+    def tournament_selection(self, population: List, fitnesses: List[Tuple[float, float]], ranks: List[int], distances: List[float]) -> Tuple:
+        """Select parent using Pareto rank and crowding distance"""
+        tournament_indices = random.sample(range(len(population)), self.tournament_size)
+        sorted_indices = sorted(tournament_indices, key=lambda x: (ranks[x], -distances[x]))
+        return population[sorted_indices[0]]
+
+    def calculate_Euclidean_distence(self,front,fitness):
+        distance = [0] * len(front)
+        for i,idx in enumerate(front):
+            distance[i]=np.linalg.norm([-fitness[idx][0],fitness[idx][1]])
+        return distance
+
+    def calculate_the_best_solution(self,front,population,fitness,best_tour,best_picking,best_profit,best_time):
+        distances = self.calculate_Euclidean_distence(front, fitness)
+        sorted_distances = sorted(range(len(front)), key=lambda x: distances[x])
+        sorted_front = [front[i] for i in sorted_distances]
+        idx = sorted_front[0]
+        profit = fitness[idx][0]
+        time = fitness[idx][1]
+        tour = population[idx][0]
+        picking = population[idx][1]
+        return (tour,picking,profit,time) if self.dominated(profit,best_profit,time,best_time) else (best_tour,best_picking,best_profit,best_time)
+
+    def solve(self) -> List[Tuple[float, float]]:
+        """Main NSGA-II algorithm loop"""
+        population = self.create_initial_population()
+        best_idx= 0
+        best_tour = population[best_idx][0]
+        best_picking = population[best_idx][1]
+        best_profit = 0
+        best_time = float('inf')
+
+        a = []
+        b = []
+        for generation in range(self.generations):
+            fitnesses = [self.evaluate_solution(tour, picking) for tour, picking in population]
+            fronts = self.fast_nondominated_sort(population, fitnesses)
+
+            ranks = [0] * len(population)
+            for rank, front in enumerate(fronts):
+                for idx in front:
+                    ranks[idx] = rank
+
+            distances = [0] * len(population)
+            for front in fronts:
+                front_distances = self.calculate_crowding_distance(front, fitnesses)
+                for i, idx in enumerate(front):
+                    distances[idx] = front_distances[i]
+
+            best_tour,best_picking,best_profit,best_time = self.calculate_the_best_solution(
+                fronts[0], population,fitnesses,best_tour,best_picking,best_profit,best_time
+            )
+
+            new_population = []
+            new_population.append(population[fronts[0][0]])
+
+            while len(new_population) < self.pop_size:
+                parent1 = self.tournament_selection(population, fitnesses, ranks, distances)
+                parent2 = self.tournament_selection(population, fitnesses, ranks, distances)
+
+                offspring_tour = self.crossover_tour(parent1[0], parent2[0])
+                offspring_picking = self.crossover_picking(parent1[1], parent2[1])
+
+                offspring_tour = self.mutate_tour(offspring_tour)
+                offspring_picking = self.mutate_picking(offspring_picking)
+
+                new_population.append((offspring_tour, offspring_picking))
+
+            new_population_fitnesses = [self.evaluate_solution(tour, picking) for tour, picking in new_population]
+            new_fronts = self.fast_nondominated_sort(new_population, new_population_fitnesses)
+
+            next_population = []
+            for front in new_fronts:
+                if len(next_population) + len(front) <= self.pop_size:
+                    next_population.extend(front)
+                else:
+                    distances = self.calculate_crowding_distance(front, new_population_fitnesses)
+                    sorted_distances = sorted(range(len(front)), key=lambda x: -distances[x])
+                    sorted_front = [front[i] for i in sorted_distances]
+                    next_population.extend(sorted_front[:self.pop_size - len(next_population)])
+                    break
+
+            population = [new_population[i] for i in next_population]
+
+            if generation % 10 == 0:
+                best_tour, best_picking, best_profit, best_time = self.calculate_the_best_solution(
+                    fronts[0], population, fitnesses, best_tour, best_picking, best_profit, best_time
+                )
+                print(f"Generation {generation} complete,Best profit:{best_profit},Best time:{best_time}")
+
+
+        # final_fitnesses = [self.evaluate_solution(tour, picking) for tour, picking in population]
+        # return final_fitnesses
+        return best_tour, best_picking, best_profit, best_time
 
     def create_initial_population(self) -> List[Tuple[List[int], List[bool]]]:
         """Create initial population of tours and picking plans"""
@@ -154,46 +262,29 @@ class NEGA2:
         cities = list(range(2, self.dimension + 1))
 
         for _ in range(self.pop_size):
-            # Generate random tour
             tour = cities.copy()
             random.shuffle(tour)
             tour = [1] + tour
-            # Generate random picking plan
             picking_plan = [random.random() < 0.5 for _ in range(len(self.items))]
-
             population.append((tour, picking_plan))
 
         return population
-
-    def tournament_selection(self, population: List, fitness_values: List[float]) -> Tuple:
-        """Select parent using tournament selection"""
-        tournament_indices = random.sample(range(len(population)), self.tournament_size)
-        tournament_fitness = [fitness_values[i] for i in tournament_indices]
-        winner_idx = tournament_indices[np.argmax(tournament_fitness)]
-        return population[winner_idx]
 
     def crossover_tour(self, parent1: List[int], parent2: List[int]) -> List[int]:
         """Order crossover (OX) for tour"""
         p1_rest = parent1[1:]
         p2_rest = parent2[1:]
         size = len(p1_rest)
-        # Select substring
         start, end = sorted(random.sample(range(size), 2))
 
-        # Initialize offspring with parent1 substring
         offspring = [0] * size
-        off_condition = [True] * size
         offspring[start:end] = p1_rest[start:end]
 
-        # Fill remaining positions from parent2
         current_pos = end
         for city in p2_rest[end:] + p2_rest[:end]:
             if city not in offspring:
                 offspring[current_pos] = city
                 current_pos = (current_pos + 1) % size
-                off_condition[current_pos] = False
-                if current_pos == start:
-                    break
 
         return [1] + offspring
 
@@ -204,69 +295,26 @@ class NEGA2:
     def mutate_tour(self, tour: List[int]) -> List[int]:
         """Swap mutation for tour"""
         if random.random() < self.mutation_rate:
-            i, j = random.sample(range(1,len(tour)), 2)
+            i, j = random.sample(range(1, len(tour)), 2)
             tour[i], tour[j] = tour[j], tour[i]
         return tour
 
     def mutate_picking(self, picking_plan: List[bool]) -> List[bool]:
         """Bit flip mutation for picking plan"""
-        return [not bit if random.random() < self.mutation_rate else bit
-                for bit in picking_plan]
+        return [not bit if random.random() < self.mutation_rate else bit for bit in picking_plan]
 
-    def solve(self) -> Tuple[List[int], List[bool], float]:
-        """Main NEGA2 algorithm loop"""
-        # Initialize population
-        population = self.create_initial_population()
-        best_solution = None
-        best_fitness = float('-inf')
 
-        for generation in range(self.generations):
-            # Evaluate current population
-            fitness_values = [self.evaluate_solution(tour, picking)
-                              for tour, picking in population]
+# Example usage
+if __name__ == "__main__":
+    file_path = "data_resources/a280-n279.txt"
+    metadata, city_coords, city_items = load_all_data(file_path)
+    city_coords = pd.DataFrame.from_dict(city_coords, orient='index', columns=['x', 'y'])
+    items_info = pd.DataFrame(city_items, columns=['item_id', 'city_id', 'value', 'weight'])
 
-            # Update best solution
-            max_idx = np.argmax(fitness_values)
-            if fitness_values[max_idx] > best_fitness:
-                best_fitness = fitness_values[max_idx]
-                best_solution = population[max_idx]
+    solver = NSGA2(metadata, city_coords, items_info)
+    best_tour, best_picking, best_profit, best_time = solver.solve()
 
-            # Create new population
-            new_population = []
-
-            while len(new_population) < self.pop_size:
-                # Select parents
-                parent1_tour, parent1_picking = self.tournament_selection(
-                    population, fitness_values)
-                parent2_tour, parent2_picking = self.tournament_selection(
-                    population, fitness_values)
-
-                # Create offspring
-                offspring_tour = self.crossover_tour(parent1_tour, parent2_tour)
-                offspring_picking = self.crossover_picking(
-                    parent1_picking, parent2_picking)
-
-                # Mutate offspring
-                offspring_tour = self.mutate_tour(offspring_tour)
-                offspring_picking = self.mutate_picking(offspring_picking)
-
-                new_population.append((offspring_tour, offspring_picking))
-
-            population = new_population
-
-            if generation % 10 == 0:
-                print(f"Generation {generation}, Best fitness: {best_fitness}")
-
-        return best_solution[0], best_solution[1], best_fitness
-
-# Create solver and solve
-solver = NEGA2(metadata, city_coords, items_info)
-best_tour, best_picking, best_fitness = solver.solve()
-
-print("\nBest solution found:")
-print(f"Tour: {best_tour}")
-print("Picked items:")
-for i, (picked, item) in enumerate(zip(best_picking, items_info.itertuples())):
-    if picked:
-        print(f"Item {item.item_id} at city {item.city_id}: value={item.value}, weight={item.weight}")
-print(f"Total fitness: {best_fitness}")
+    print(f"Best profit:{best_profit},Best time:{best_time}")
+    # print("\nFinal Pareto front:")
+    # for profit, time in final_front:
+    #     print(f"Profit: {profit}, Time: {time}")
